2a4ed9f0553a90e2c28bb57c2a28cd875e67347f
mzak21 <zakrevsky843@gmail.com>
2024-01-18 19:35:52 -0800

    Add Addresses class with distances and numerics Add testing code to call total_distance and get_address Add Addresses.csv to get string literals of addresses

---
+++ Addresses.py
@@ -0,0 +1,35 @@
+import csv
+from math import dist
+
+
+class Addresses:
+
+    #open the designated csv file as read only ('r')
+    #search through the rows until they find the matching string literal
+    # return the farthest value to the left to represent the address
+    def get_address(self, address, address_csv):
+        with open(address_csv, 'r') as address_info:
+            csv_reader = csv.reader(address_info)
+            for row in csv_reader:
+                if len(row) >= 3 and address in row[1]:
+                    return int(row[0])
+    
+    #open our Distance csv file and find the intersection of two given addresses
+    #utilizes the get_address method above to get our values
+    # example: "Westerns Governors University" = 0 and "Internation Peace Gardens" = 1
+    # should return a distance intersection of "7.2"
+    def total_distance(self, address_1, address_2, distance_csv):
+        with open(distance_csv, 'r') as distance_info:
+            csv_reader = csv.reader(distance_info)
+            column_index = int(address_1)
+            row_index = int(address_2)
+
+            for i, row in enumerate(csv_reader):
+                if i == row_index:
+                    distance = row[column_index]
+                    if distance == '':
+                        distance = 0
+
+                    return float(distance)
+            
+            return None
--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import datetime
 import TruckClass
 
 from InstantiateHashMap import InstantiateHashMap
+from Addresses import Addresses
 
 
 #create our instance of the hash table
@@ -19,3 +20,22 @@ for pkg_id in range(1, 41):
      package = packages_hash.get(pkg_id)
      print(str(package))
 
+#Call our Addresses class to find a specific address within the CSV
+#Then return the value assigned to represent the address
+# example: "Westerns Governors University" returns 0
+print("-----------Address values--------")
+
+address = "Sugar House Park" #should return the number "2"
+address_csv = Addresses()
+print(address_csv.get_address(address, "csv/WGUPS_Addresses_csv.csv"))
+
+
+#Assign our test values for addresses and then return the intersection representing distance between
+# uses the total_distance method in Addresses class
+# example: "Westerns Governors University" = 0 and "Internation Peace Gardens" = 1
+# should return a distance intersection of "7.2"
+print("-----------Distance--------")
+
+address_val_1 = 0
+address_val_2 = 1
+print(address_csv.total_distance(address_val_1, address_val_2, "csv/WGUPS_Distance_csv.csv"))
